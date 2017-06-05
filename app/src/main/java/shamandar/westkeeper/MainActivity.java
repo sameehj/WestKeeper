35fee8dc343f48ca90271778c585159e4c0de9db
Aysar Shahwan <ayser_sh@hotmail.com>
2017-06-05 23:42:19 +0300

    Scores Activity Indeed

--- app/src/main/java/shamandar/westkeeper/MainActivity.java
+++ app/src/main/java/shamandar/westkeeper/MainActivity.java
@@ -2,6 +2,7 @@ package shamandar.westkeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView team1player1 = (TextView)findViewById(R.id.editText);
+        TextView team1player2 = (TextView)findViewById(R.id.editText2);
+        TextView team2player1 = (TextView)findViewById(R.id.editText3);
+        TextView team2player2 = (TextView)findViewById(R.id.editText4);
     }
 }
